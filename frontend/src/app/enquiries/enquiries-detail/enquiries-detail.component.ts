db430cc027c60aa0747c0982d50283ce16bc36e3
eevan7a9 <e7a9@protonmail.com>
2021-07-31 14:33:41 +0900

    perf(enquiries): added delete

--- frontend/src/app/enquiries/enquiries-detail/enquiries-detail.component.ts
+++ frontend/src/app/enquiries/enquiries-detail/enquiries-detail.component.ts
@@ -1,6 +1,8 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
+
 import { Enquiry } from 'src/app/shared/interface/enquiries';
 import { EnquiriesService } from '../enquiries.service';
 
@@ -15,7 +17,9 @@ export class EnquiriesDetailComponent implements OnInit {
   constructor(
     public location: Location,
     private router: Router,
-    private enquiriesService: EnquiriesService
+    private enquiriesService: EnquiriesService,
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController
   ) { }
 
   ngOnInit() {
@@ -27,4 +31,42 @@ export class EnquiriesDetailComponent implements OnInit {
     });
   }
 
+  public async delete(enqId: string) {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-alert-class',
+      header: 'Delete Enquiry',
+      // subHeader: 'Subtitle',
+      message: 'Are you sure you want to delete this Enquiry?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            // console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'DELETE',
+          cssClass: 'alert-danger-text',
+          role: 'delete',
+          handler: () => {
+            this.enquiriesService.removeEnquiry(enqId);
+            this.router.navigate(['/enquiries']);
+            this.presentToast('Enquiry is deleted successfully.');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    // const { role } = await alert.onDidDismiss();
+  }
+
+  async presentToast(message: string, duration = 3000) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration,
+      color: 'success'
+    });
+    toast.present();
+  }
 }
--- frontend/src/app/enquiries/enquiries.service.ts
+++ frontend/src/app/enquiries/enquiries.service.ts
@@ -8,17 +8,32 @@ import { Enquiry } from '../shared/interface/enquiries';
 export class EnquiriesService {
 
   public readonly enquiries$: Observable<Enquiry[]>;
+  public readonly enquiry$: Observable<Enquiry>;
   private readonly enquiriesSub = new BehaviorSubject<Enquiry[]>([]);
+  private readonly enquirySub = new BehaviorSubject<Enquiry>(null);
 
   constructor() {
     this.enquiries$ = this.enquiriesSub.asObservable();
+    this.enquiry$ = this.enquirySub.asObservable();
   }
 
   public get enquiries(): Enquiry[] {
     return this.enquiriesSub.getValue();
   }
 
-  public set enquiries(property: Enquiry[]) {
-    this.enquiriesSub.next(property);
+  public set enquiries(enquiries: Enquiry[]) {
+    this.enquiriesSub.next(enquiries);
+  }
+
+  public get enquiry(): Enquiry | null {
+    return this.enquirySub.getValue();
+  }
+
+  public set enquiry(enquiry: Enquiry) {
+    this.enquirySub.next(enquiry);
+  }
+
+  public removeEnquiry(enqId: string) {
+    this.enquiries = this.enquiries.filter(enquiry => enquiry.enqId !== enqId);
   }
 }
