cb697315d8f2494d112dfc5076deb12a8c0511ad
eevan7a9 <e7a9@protonmail.com>
2021-08-03 23:23:12 +0900

    feat(map): property-badge

--- frontend/src/app/map/map.module.ts
+++ frontend/src/app/map/map.module.ts
@@ -10,13 +10,15 @@ import { MapPage } from './map.page';
 import { PropertiesListComponent } from '../properties/properties-list/properties-list.component';
 import { MapLeafletComponent } from './map-leaflet/map-leaflet.component';
 import { MapPopupComponent } from './map-popup/map-popup.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MapPageRoutingModule
+    MapPageRoutingModule,
+    SharedModule
   ],
   declarations: [MapPage, PropertiesListComponent, MapLeafletComponent, MapPopupComponent]
 })
