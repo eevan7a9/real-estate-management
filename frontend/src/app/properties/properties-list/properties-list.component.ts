e53a17cad1cd1c3e7dfe2c13f476bd000e7d90f7
eevan7a9 <e7a9@protonmail.com>
2021-08-03 20:15:36 +0900

    perf(dummy-data): move dummies dir

--- frontend/src/app/properties/properties-list/properties-list.component.ts
+++ frontend/src/app/properties/properties-list/properties-list.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Property } from 'src/app/shared/interface/property';
 import { PropertiesService } from '../properties.service';
-
+import { properties } from '../../shared/dummy-data';
 @Component({
   selector: 'app-properties-list',
   templateUrl: './properties-list.component.html',
@@ -20,46 +20,6 @@ export class PropertiesListComponent implements OnInit {
   ngOnInit() {
     this.propertiesService.properties$.subscribe(v => {
       this.properties = v;
-      if (!this.properties.length) {
-        this.propertiesService.properties = [
-          {
-            id: '01',
-            address: 'Some place free',
-            description: 'And this is nice',
-            name: 'Property A',
-            type: 'house'
-          },
-          {
-            id: '02',
-            address: 'Some place free',
-            description: 'And this is nice',
-            name: 'Property B',
-            type: 'apartment'
-          },
-          {
-            id: '03',
-            address: 'Some place free',
-            description: 'And this is nice',
-            name: 'Property C',
-            type: 'pad'
-          },
-          {
-            id: '04',
-            address: 'Some place free',
-            description: 'And this is nice',
-            name: 'Property D',
-            type: 'pad'
-          },
-          {
-            id: '05',
-            address: 'Some place free',
-            description: 'And this is nice',
-            name: 'Property E',
-            type: 'boardingHouse'
-          }
-        ];
-        this.properties = this.propertiesService.properties;
-      }
     });
   }
 
---
+++ frontend/src/app/shared/dummy-data.ts
@@ -0,0 +1,59 @@
+import { Property } from './interface/property';
+
+export const properties: Property[] = [
+  {
+    id: '01',
+    address: 'Some place free',
+    description: 'And this is nice',
+    name: 'Property A',
+    type: 'house',
+    position: {
+      lat: 8.948677279926585,
+      lng: 125.5470567303216
+    }
+  },
+  {
+    id: '02',
+    address: 'Some place free',
+    description: 'And this is nice',
+    name: 'Property B',
+    type: 'apartment',
+    position: {
+      lat: 8.948603092201049,
+      lng: 125.54424652989282
+    }
+  },
+  {
+    id: '03',
+    address: 'Some place free',
+    description: 'And this is nice',
+    name: 'Property C',
+    type: 'pad',
+    position: {
+      lat: 8.947998991587234,
+      lng: 125.5430559487951
+    }
+  },
+  {
+    id: '04',
+    address: 'Some place free',
+    description: 'And this is nice',
+    name: 'Property D',
+    type: 'pad',
+    position: {
+      lat: 8.947055744763734,
+      lng: 125.5442679818045
+    }
+  },
+  {
+    id: '05',
+    address: 'Some place free',
+    description: 'And this is nice',
+    name: 'Property E',
+    type: 'boardingHouse',
+    position: {
+      lat: 8.946536428053331,
+      lng: 125.54334554960266
+    }
+  }
+];
