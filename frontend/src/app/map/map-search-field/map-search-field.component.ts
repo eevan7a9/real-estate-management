117ee5c8af30fc89806e7058caca35f2bd12f7cf
eevan7a9 <e7a9@protonmail.com>
2021-08-08 01:20:08 +0900

    perf(map-search): delete input

--- frontend/src/app/map/map-search-field/map-search-field.component.ts
+++ frontend/src/app/map/map-search-field/map-search-field.component.ts
@@ -14,6 +14,7 @@ import { Coord } from 'src/app/shared/interface/map';
 export class MapSearchFieldComponent implements OnInit {
 
   @Output() selectedLocation = new EventEmitter<Coord>();
+  public field = '';
 
   constructor(private modalCtrl: ModalController) { }
 
@@ -31,6 +32,7 @@ export class MapSearchFieldComponent implements OnInit {
       }
     });
     await modal.present();
+    this.field = '';
     const { data } = await modal.onDidDismiss();
     if (data) {
       const { lat, lng } = data;
