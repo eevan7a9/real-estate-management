a6fda9d5db550cdd5be25caeb3ae62d829672e99
eevan7a9 <e7a9@protonmail.com>
2021-08-16 11:05:16 +0900

    feat(app): about page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -34,6 +34,10 @@ const routes: Routes = [
   {
     path: 'user',
     loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
   }
 ];
 
