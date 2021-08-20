d7af904d7da3afb7bc45b03bd15cb36c15eb2ae8
eevan7a9 <e7a9@protonmail.com>
2021-08-20 15:07:59 +0900

    perf(properties-list): added infinit scrolling

--- frontend/src/app/properties/properties-list/properties-list.component.ts
+++ frontend/src/app/properties/properties-list/properties-list.component.ts
@@ -15,6 +15,8 @@ export class PropertiesListComponent implements OnInit, OnDestroy {
   @Input() horizontalSlide = false;
   @Input() limit = 0;
   public properties: Property[];
+  public displayedItems: Property[] = [];
+  public maxDisplayed = 8;
   private unsubscribe$ = new Subject<void>();
 
   constructor(
@@ -27,6 +29,7 @@ export class PropertiesListComponent implements OnInit, OnDestroy {
     this.propertiesService.properties$.pipe(takeUntil(this.unsubscribe$)).subscribe(v => {
       this.properties = this.limit ? v.slice(0, this.limit) : v;
     });
+    this.displayedItems = this.properties.slice(0, this.maxDisplayed);
     this.changeDetector.detectChanges();
   }
 
@@ -39,4 +42,20 @@ export class PropertiesListComponent implements OnInit, OnDestroy {
     this.propertiesService.property = property;
     this.router.navigate(['/properties', property.id]);
   }
+
+  loadData(event) {
+    this.maxDisplayed = this.maxDisplayed + 4;
+    setTimeout(() => {
+      this.displayedItems = this.properties.slice(0, this.maxDisplayed);
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.displayedItems.length >= this.properties.length) {
+        console.log('everything is loaded');
+        event.target.disabled = true;
+      }
+    }, 1500);
+  }
+
 }
