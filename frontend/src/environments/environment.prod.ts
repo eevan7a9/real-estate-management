0869274890b8fd4d4e82c704ec3c9e6232cc1d8f
eevan7a9 <e7a9@protonmail.com>
2021-08-28 03:39:57 +0900

    perf: added url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    url: 'http://localhost:8000/',
+  }
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: {
+    url: 'http://localhost:8000/',
+  }
 };
 
 /*
