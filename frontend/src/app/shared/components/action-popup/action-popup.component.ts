a1feceff4fb7b61016236a1d778c27e68c76419d
eevan7a9 <e7a9@protonmail.com>
2021-08-31 20:40:05 +0900

    perf(actin-popup): restrict actions

--- frontend/src/app/shared/components/action-popup/action-popup.component.ts
+++ frontend/src/app/shared/components/action-popup/action-popup.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { PopoverController } from '@ionic/angular';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-action-popup',
@@ -10,12 +12,21 @@ export class ActionPopupComponent implements OnInit {
   @Input() message = true;
   @Input() edit = true;
   @Input() delete = true;
-
-  constructor(private popupCtrl: PopoverController) { }
+  constructor(
+    private popupCtrl: PopoverController,
+    private userService: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit() { }
 
   close(action = null) {
+    const user = this.userService.user;
+    if (!user) {
+      this.router.navigateByUrl('user/signin');
+      this.popupCtrl.dismiss();
+      return;
+    }
     this.popupCtrl.dismiss({ action });
   }
 }
