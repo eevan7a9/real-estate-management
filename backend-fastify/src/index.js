979ec71d302e48b9ca544254fea69a4a587cb089
eevan7a9 <e7a9@protonmail.com>
2021-09-04 14:08:50 +0900

    feat(app): API documentation swagger

--- backend-fastify/src/index.js
+++ backend-fastify/src/index.js
@@ -3,6 +3,7 @@ import Fastify from "fastify";
 import FastifyBcrypt from "fastify-bcrypt";
 import FastifyJwt from "fastify-jwt";
 import FastifyCors from "fastify-cors";
+import FastifySwagger from "fastify-swagger";
 // Local Files
 import { usersRoutes } from "./routes/users.js";
 import { authRoutes } from "./routes/auth.js";
@@ -12,6 +13,21 @@ const PORT = process.env.PORT || 5000;
 
 export const fastify = await Fastify({ logger: process.env.LOGGER || true });
 
+fastify.register(FastifySwagger, {
+  exposeRoute: true,
+  routePrefix: "/docs",
+  swagger: {
+    info: {
+      title: "API Documentation",
+      description: "Fastify swagger API documentation.",
+      version: "0.1.0",
+    },
+    externalDocs: {
+      url: "https://swagger.io",
+      description: "Find more info here",
+    },
+  },
+});
 fastify.register(FastifyJwt, { secret: process.env.SECRET_KEY || "secret" });
 fastify.register(FastifyBcrypt, {
   saltWorkFactor: Number(process.env.SALT) || 12,
