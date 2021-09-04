92c931cb82e43f189dd0885bc14b4677fe6571b8
eevan7a9 <e7a9@protonmail.com>
2021-09-04 16:06:01 +0900

    change url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api: {
-    url: 'http://localhost:8000/',
+    url: 'https://rem-db.glitch.me/',
   }
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   api: {
-    url: 'http://localhost:8000/',
+    url: 'https://rem-db.glitch.me/',
   }
 };
 
