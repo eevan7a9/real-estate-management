3efd4fc3af791d480ea9a4a19781c7ca14841ac6
eevan7a9 <e7a9@protonmail.com>
2021-09-05 23:30:38 +0900

    style(app): added footer

--- frontend/src/app/settings/settings.module.ts
+++ frontend/src/app/settings/settings.module.ts
@@ -9,13 +9,15 @@ import { SettingsPageRoutingModule } from './settings-routing.module';
 import { SettingsPage } from './settings.page';
 import { SettingsThemeComponent } from './settings-theme/settings-theme.component';
 import { SettingsCoordDefaultComponent } from './settings-coord-default/settings-coord-default.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SettingsPageRoutingModule
+    SettingsPageRoutingModule,
+    SharedModule
   ],
   declarations: [SettingsPage, SettingsThemeComponent, SettingsCoordDefaultComponent]
 })
