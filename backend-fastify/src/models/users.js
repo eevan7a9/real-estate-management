48fd52bb6b0251305af62074698754131fc01aa4
eevan7a9 <e7a9@protonmail.com>
2021-10-29 20:50:29 +0900

    perf(users): models

---
+++ backend-fastify/src/models/users.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: String,
+      required: true,
+    },
+    fullName: {
+      type: String,
+      minlength: 4,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: /.+\@.+\..+/,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
+
+export const userAuthResponse = {
+  id: { type: "string" },
+  user_id: { type: "string" },
+  email: { type: "string" },
+  fullName: { type: "string" },
+  accessToken: { type: "string" },
+  password: { type: "string" },
+};
