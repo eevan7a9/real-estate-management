1309e9692e8a5709ac6f274ae9a550076809d95c
eevan7a9 <e7a9@protonmail.com>
2021-10-30 20:26:02 +0900

    perf(routes): change to options

--- backend-fastify/src/routes/options/properties.js
+++ backend-fastify/src/routes/options/properties.js
@@ -51,3 +51,20 @@ export const createPropertyOpts = (handler) => ({
   },
   handler: handler,
 });
+
+export const deletePropertyOpts = (handler) => ({
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          message: { type: "string" },
+          property_id: { type: "string" },
+          name: { type: "string" },
+          description: { type: "string" },
+        },
+      },
+    },
+  },
+  handler: handler,
+});
