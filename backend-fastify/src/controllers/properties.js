f4b9a529c414256bfccf2c997093cc54fca1ea28
eevan7a9 <e7a9@protonmail.com>
2021-10-30 16:47:45 +0900

    perf(properties): update get properties

--- backend-fastify/src/controllers/properties.js
+++ backend-fastify/src/controllers/properties.js
@@ -1,49 +1,31 @@
-import { properties } from "../dummy-data/properties.js";
 import { Property } from "../models/property.js";
 import { v4 as uuidv4 } from "uuid";
-export const getProperties = function (_, res) {
+
+export const getProperties = async function (_, res) {
+  const properties = await Property.find();
   res.status(200).send(properties);
 };
 
-export const getProperty = function (req, res) {
-  const findProperty = properties.find(
-    (property) => property.id == req.params.id
-  );
-  if (findProperty) {
-    res.status(200).send(findProperty);
+export const getProperty = async function (req, res) {
+  const { id } = req.params;
+  try {
+    const property = await Property.findOne({ property_id: id });
+    res.status(200).send(property);
+  } catch (error) {
+    res.status(404).send({ message: "Error: Can't find property." });
   }
 };
 
 export const createProperty = async function (req, res) {
-  const {
-    name,
-    address,
-    description,
-    type,
-    position,
-    price,
-    date,
-    enquiries,
-    currency,
-    features,
-  } = req.body;
+  const { name, address, type, position } = req.body;
   if (!name || !address || !type || !position) {
-    res.status(400).send({ message: "Required fields are missing" });
+    res.status(400).send({ message: "Error: Required fields are missing." });
     return;
   }
   try {
     const newProperty = new Property({
       property_id: uuidv4(),
-      name,
-      address,
-      description,
-      type,
-      position,
-      price,
-      date,
-      enquiries,
-      currency,
-      features,
+      ...req.body,
     });
     await newProperty.save();
     res.status(201).send(newProperty);
