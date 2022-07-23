dbd8ea0bec93def423574c92b2feb58f5d462338
eevan7a9 <e7a9@protonmail.com>
2022-07-23 17:34:41 +0800

    perf(users): remove getMeOpts

--- backend-fastify/src/routes/users/options/index.js
+++ backend-fastify/src/routes/users/options/index.js
@@ -1,5 +1,4 @@
 import { getUsersOpts } from "./get-users.js";
 import { getUserOpts } from "./get-user.js";
-import { getMeOpts } from './get-me.js';
 
-export { getUsersOpts, getUserOpts, getMeOpts };
+export { getUsersOpts, getUserOpts };
