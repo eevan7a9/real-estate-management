daa03c761e2a70657a12bd6ea7207f46d4d3f781
eevan7a9 <e7a9@protonmail.com>
2022-07-23 17:54:12 +0800

    perf(models): update enquiries schema

--- backend-fastify/src/models/enquiry.js
+++ backend-fastify/src/models/enquiry.js
@@ -1,15 +1,26 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  from: { type: String, required: true },
-  to: { type: String, required: true },
+const propertySchema = new mongoose.Schema({
+  property_id: { type: String },
+  name: { type: String },
+});
+
+const usersSchema = new mongoose.Schema({
+  from: {
+    user_id: { type: String },
+    keep: { type: Boolean, default: true, required: true }
+  },
+  to: {
+    user_id: { type: String },
+    keep: { type: Boolean, default: true, required: true }
+  }
 });
 
 const replyToSchema = new mongoose.Schema({
   id: { type: String },
   title: { type: String },
   topic: { type: String },
-})
+});
 
 const enquirySchema = new mongoose.Schema(
   {
@@ -18,13 +29,10 @@ const enquirySchema = new mongoose.Schema(
     email: { type: String },
     title: { type: String, required: true },
     topic: { type: String, required: true },
-    read: { type: Boolean },
-    property: {
-      name: { type: String },
-      id: { type: String },
-    },
-    user: userSchema,
+    read: { type: Boolean, default: false },
+    property: propertySchema,
     replyTo: replyToSchema,
+    users: usersSchema
   },
   {
     timestamps: true,
