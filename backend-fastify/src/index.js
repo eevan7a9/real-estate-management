0b5f024533c303aaa93f6d57c1887d57d5568e72
eevan7a9 <e7a9@protonmail.com>
2023-06-18 19:56:53 +0800

    feat: added websocket connection

--- backend-fastify/src/index.js
+++ backend-fastify/src/index.js
@@ -4,13 +4,21 @@ import FastifyBcrypt from "fastify-bcrypt";
 import FastifyJwt from "@fastify/jwt";
 import FastifyMultipart from "@fastify/multipart";
 import mongoose from "mongoose";
+import FastifyWebsocket from '@fastify/websocket';
+
 // Local Files
 import { setFastifySwagger } from "./swagger.js";
 import { setFastifyCors } from "./cors.js";
 import { setFastifyRoutes } from "./routes/index.js";
 import { setFastifyStatic } from "./static.js";
+import { setFastifyWebsocket } from "./websocket/index.js"
 
 dotenv.config();
+
+/**
+ * The Fastify instance.
+ * @type {import('fastify').FastifyInstance}
+ */
 export const fastify = await Fastify({ logger: process.env.LOGGER || true });
 
 // We allow Multi Part Form
@@ -21,6 +29,9 @@ fastify.register(FastifyJwt, { secret: process.env.SECRET_KEY || "secret" });
 fastify.register(FastifyBcrypt, {
   saltWorkFactor: Number(process.env.SALT) || 12,
 });
+// We register Websocket
+fastify.register(FastifyWebsocket);
+
 // We register authenticate
 fastify.decorate("authenticate", async function (request, reply) {
   try {
@@ -35,8 +46,10 @@ setFastifySwagger(fastify);
 setFastifyStatic(fastify);
 // We allowed cors
 setFastifyCors(fastify);
-//  We register routes
+// We register routes
 setFastifyRoutes(fastify);
+// We set webSocket connection
+setFastifyWebsocket();
 
 mongoose
   .connect(process.env.DB_CONNECT, {
